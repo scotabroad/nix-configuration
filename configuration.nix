a61e67d1983844323e645058a685783f75327890
Liam Petrie <liamdpetrie@vivaldi.net>
2022-07-29 15:02:37 -0700

    Set tty colors to Nord scheme

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,24 @@ in {
   # boot.loader.grub.device = "nodev"; # "nodev" for efi only
   boot.loader.systemd-boot.enable = true;
 
+  console.colors = [
+    "3b4252"
+    "bf616a"
+    "a3be8c"
+    "ebcb8b"
+    "81a1c1"
+    "b48ead"
+    "88c0d0"
+    "e5e9f0"
+    "4c566a"
+    "bf616a"
+    "ebcb8b"
+    "81a1c1"
+    "b48ead"
+    "8fbcbb"
+    "eceff4"
+  ];
+
   #Enable Zram
   zramSwap = {
     enable = true;
