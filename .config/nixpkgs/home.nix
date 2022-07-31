85315cd18b0cfaacf60b68113363683b95cfa512
Liam Petrie <liamdpetrie@vivaldi.net>
2022-07-30 22:05:55 -0700

    Moved zsh configuration into separate file

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -22,47 +22,15 @@ let
     fi
   '';
 in {
+
+  imports = [
+    ./programs/zsh
+  ];
+
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "liamdp";
   home.homeDirectory = "/home/liamdp";
-
-  # Manage Zsh
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      rm = "rm -i";
-    };
-    zplug = {
-      enable = true;
-      plugins = [
-	{ name = "lib/clipboard"; tags = [ from:oh-my-zsh ]; }
-	{ name = "lib/history"; tags = [ from:oh-my-zsh ]; }
-	{ name = "lib/key-bindings"; tags = [ from:oh-my-zsh ]; }
-	{ name = "lib/theme-and-appearance"; tags = [ from:oh-my-zsh ]; }
-        { name = "plugins/command-not-found"; tags = [ from:oh-my-zsh ]; }
-	{ name = "plugins/sudo"; tags = [ from:oh-my-zsh ]; }
-	{ name = "ChrisPenner/copy-pasta"; }
-	{ name = "zpm-zsh/ls"; }
-	{ name = "zsh-users/zsh-syntax-highlighting"; }
-	{ name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
-      ];
-    };
-    defaultKeymap = "emacs"; #emacs, vicmd, or viins (vim mode bad)
-    history = {
-      size = 5000;
-      extended = true;
-      ignoreDups = true;
-      expireDuplicatesFirst = true;
-      ignoreSpace = true;
-      save =5000;
-      path = "/home/liamdp/.zsh_history";
-    };
-    initExtra = ''
-      export PATH=$PATH:/home/liamdp/bin
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-    '';
-  };
 
   # Manage Neovim
   programs.neovim = {
---
+++ .config/nixpkgs/programs/zsh/default.nix
@@ -0,0 +1,38 @@
+{ config, inputs, pkgs, home-manager, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      rm = "rm -i";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "lib/clipboard"; tags = [ from:oh-my-zsh ]; }
+	{ name = "lib/history"; tags = [ from:oh-my-zsh ]; }
+	{ name = "lib/key-bindings"; tags = [ from:oh-my-zsh ]; }
+	{ name = "lib/theme-and-appearance"; tags = [ from:oh-my-zsh ]; }
+	{ name = "plugins/command-not-found"; tags = [ from:oh-my-zsh ]; }
+	{ name = "plugins/sudo"; tags = [ from:oh-my-zsh ]; }
+	{ name = "ChrisPenner/copy-pasta"; }
+	{ name = "zpm-zsh/ls"; }
+	{ name = "zsh-users/zsh-syntax-highlighting"; }
+	{ name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+      ];
+    };
+    defaultKeymap = "emacs"; #emacs, vicmd, or viins (vim mode bad)
+    history = {
+      size = 5000;
+      extended = true;
+      ignoreDups = true;
+      expireDuplicatesFirst = true;
+      save = 5000;
+      path = "/home/liamdp/.zsh_history";
+    };
+    initExtra = ''
+      export PATH=$PATH:/hoe/liamdp/bin
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
+  };
+}
