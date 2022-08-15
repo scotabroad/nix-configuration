c21e2b0399e81630ecb331258677da0e8acc9960
Liam Petrie <liamdpetrie@vivaldi.net>
2022-08-15 10:02:00 -0700

    Fixed Discord bugs with opening links

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 final: prev: {
-  discord = prev.discord.overrideAttrs (_: {
+  discord = (prev.discord.override { nss = pkgs.nss_latest;}).overrideAttrs (_: {
     src = builtins.fetchTarball {
       url = "https://discord.com/api/download?platform=linux&format=tar.gz";
       sha256 = "1kwqn1xr96kvrlbjd14m304g2finc5f5ljvnklg6fs5k4avrvmn4";
