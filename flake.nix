cc3c1600001396f7fb5b351a206142b1a7ab9bd0
Liam Petrie <liamdpetrie@vivaldi.net>
2022-08-21 20:02:42 -0700

    More improvements in getting taffybar set up

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "Liam Petrie's Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager/release-22.05";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-matlab = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "gitlab:doronbehar/nix-matlab";
@@ -19,7 +20,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, nix-matlab, taffybar, ... }:
+  outputs = { self, flake-utils, nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, nix-matlab, taffybar, ... }:
     let
       system = "x86_64-linux";
 
@@ -47,6 +48,8 @@
               steam = unstable.steam;
 	      vivaldi = unstable.vivaldi;
             })
+
+	    (import ./taffybar/overlay.nix)
 
 	    nix-matlab.overlay
 
