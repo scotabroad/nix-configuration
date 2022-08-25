66d6a907f92961e8b7637be2970fd12d11987df2
Liam Petrie <liamdpetrie@vivaldi.net>
2022-08-25 11:48:32 -0700

    Removed unused stack file

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -7,23 +7,27 @@ import System.Taffybar.Widget
 import System.Taffybar.Widget.Battery
 import System.Taffybar.Widget.Generic.Graph
 import System.Taffybar.Widget.Generic.PollingGraph
+import System.Taffybar.Widget.Layout
 import System.Taffybar.Widget.SNITray
 
-cpuCallback = do
+{- cpuCallback = do
   (_, systemLoad, totalLoad) <- cpuLoad
-  return [ totalLoad, systemLoad ]
+  return [ totalLoad, systemLoad ] -}
 
 main = do
   let {-cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5)]
                                   , graphLabel = Just "cpu"
                                   }-}
 
-      myBattery = textBatteryNew "$percentage$%" 
+      myBatteryIcon = batteryIconNew
+      myBatteryText = textBatteryNew "$percentage$%" 
       myClock = textClockNewWith defaultClockConfig
       -- myCpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      myLayout = layoutNew defaultLayoutConfig
       myWorkspaces = workspacesNew defaultWorkspacesConfig
       simpleConfig = defaultSimpleTaffyConfig
-                       { startWidgets = [ myWorkspaces ]
-                       , endWidgets = [ myBattery, myClock, sniTrayNew ]
+                       { startWidgets = [ myWorkspaces, myLayout ]
+		       , centerWidgets = [ myClock ]
+                       , endWidgets = [ myBatteryText, myBatteryIcon, sniTrayNew ]
                        }
   simpleTaffybar simpleConfig
