fa576e867b4a71f6d1ed5c12f55771440a1c7008
Liam Petrie <liamdpetrie@vivaldi.net>
2022-08-31 21:48:21 -0700

    Enabled systemd screenlocking on Cinnamon due to dbus failure

--- services/cinnamon.nix
+++ services/cinnamon.nix
@@ -4,4 +4,22 @@
   imports = [
     ./flameshot
   ];
+
+  systemd.user = {
+    services = {
+      # An attempt to get cinnamon screensaver working on NixOS with systemd
+      cinnamon-screensaver = {
+        Unit = {
+          Description = "Cinnamon Screensaver";
+	  After = ["graphical-session-pre.target"];
+	  PartOf = ["graphical-session.target"];
+        };
+        Service = {
+          ExecStart = "${pkgs.cinnamon.cinnamon-screensaver}/bin/cinnamon-screensaver";
+	  Restart = "always";
+	  RestartSec = 15;
+        };
+      };
+    };
+  };
 }
