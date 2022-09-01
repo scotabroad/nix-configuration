170e5efd3da344adcaeca9ad8cd3651a14d912c9
Liam Petrie <liamdpetrie@vivaldi.net>
2022-09-01 14:25:34 -0700

    Updated Cinnamon image

--- desktops/cinnamon/cinnamon.nix
+++ desktops/cinnamon/cinnamon.nix
@@ -24,6 +24,7 @@
 	      name = "Nordic";
 	      package = pkgs.nordic;
 	    };
+	    # extraConfig = '' enable-hidpi = on ''; Failed dpi fix
 	  };
 	};
       };
--- packages/lightdm-dpi-fix.nix
+++ packages/lightdm-dpi-fix.nix
@@ -9,6 +9,7 @@ final: prev: {
     # hidpi
     export GDK_SCALE=2
     export GDK_DPI_SCALE=0.5
+    QT_AUTO_SCREEN_SCALE_FACTOR=1
 
     # run greeter
     exec $@
