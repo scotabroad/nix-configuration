16eada333215bbdabe1e18aa71028c3eca5a4e21
Liam Petrie <liamdpetrie@vivaldi.net>
2022-10-16 15:02:58 -0700

    Fixed typo

--- theme/nord/desktops/hyprland/home/waybar/default.nix
+++ theme/nord/desktops/hyprland/home/waybar/default.nix
@@ -83,7 +83,7 @@
 
 	"custom/search" = {
 	  format = "   Applications";
-	  one-click = "fuzzel -b 2e3440e6 -t d8dee9ff -B 2 -r 5 -C 8fbcbbff -s 8fbcbbff -S 2e3440ff -i Nordzy-dark -f UbuntuMono:size=14";
+	  on-click = "fuzzel -b 2e3440e6 -t d8dee9ff -B 2 -r 5 -C 8fbcbbff -s 8fbcbbff -S 2e3440ff -i Nordzy-dark -f UbuntuMono:size=14";
 	};
       }
     ];
