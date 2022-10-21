ffafd1dccfbe83df5631c98bff82fa5b6d8eaf7c
Liam Petrie <liamdpetrie@vivaldi.net>
2022-10-21 15:18:23 -0700

    Update Discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -4,8 +4,8 @@ final: prev: {
   discord = (prev.discord.override { nss = pkgs.nss_latest;}).overrideAttrs (_: {
     src = builtins.fetchTarball {
       url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-      sha256 = "0qaczvp79b4gzzafgc5ynp6h4nd2ppvndmj6pcs1zys3c0hrabpv";
+      #sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "1pw9q4290yn62xisbkc7a7ckb1sa5acp91plp2mfpg7gp7v60zvz";
     };
-    #extraProfile = "export XCURSOR_SIZE=48"; failed XWayland fix
   });
 }
