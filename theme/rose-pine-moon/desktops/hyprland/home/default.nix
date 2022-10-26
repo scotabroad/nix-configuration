455d28e665825bb0a038f924559defd987c212f3
Liam Petrie <liamdpetrie@vivaldi.net>
2022-10-25 20:25:05 -0700

    Round the corners of fuzzel

--- theme/rose-pine-moon/desktops/hyprland/home/default.nix
+++ theme/rose-pine-moon/desktops/hyprland/home/default.nix
@@ -14,7 +14,7 @@
           col.active_border=0xffc4a7e7
           col.inactive_border=0xff2a283e
       }
-      bind=ALT,R,exec,fuzzel -b 232136e6 -t e0def4ff -B 2 -r 5 -C c4a7e7ff -s c4a7e7ff -S 232136ff -i rose-pine-moon-icons -f UbuntuMono:size=14
+      bind=ALT,R,exec,fuzzel -b 232136e6 -t e0def4ff -B 2 -r 20 -C c4a7e7ff -s c4a7e7ff -S 232136ff -i rose-pine-moon-icons -f UbuntuMono:size=14
     '';
   };
 }
