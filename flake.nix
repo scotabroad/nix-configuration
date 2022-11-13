3bc2c75dbbdb7aac22ea311febcd545053c5f7a7
Liam Petrie <liamdpetrie@vivaldi.net>
2022-11-12 19:43:45 -0700

    Flake update
    Also remove Matlab

--- flake.nix
+++ flake.nix
@@ -17,10 +17,6 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
-    nix-matlab = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "gitlab:doronbehar/nix-matlab";
-    };
   };
 
   outputs = { 
@@ -30,7 +26,6 @@
     nixpkgs-unstable, 
     home-manager, 
     nixos-hardware, 
-    nix-matlab, 
     hyprland,
     hyprpicker,
     ... 
@@ -46,17 +41,16 @@
         import ps ({
           inherit system;
           config.allowUnfree = true;
-	  config.allowBroken = true;
+          config.allowBroken = true;
         } // attrs);
 
       pkgs = make-packages nixpkgs{
         
-	overlays = [
-	    nix-matlab.overlay
-	    hyprpicker.overlays.default
-        ]
-	++ import ./overlays { inherit pkgs; }
-	++ import ./packages { inherit lib pkgs; };
+      overlays = [
+        hyprpicker.overlays.default
+      ]
+      ++ import ./overlays { inherit pkgs; }
+      ++ import ./packages { inherit lib pkgs; };
       };
 
     in {
@@ -69,12 +63,12 @@
           hyprland.nixosModules.default
 	  nixos-hardware.nixosModules.framework
           home-manager.nixosModules.home-manager {
-	      home-manager = {
-                extraSpecialArgs = { inherit inputs; };
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.liamdp = import ./theme/nord/desktops/hyprland/home/default.nix;
-	     };
+	    home-manager = {
+              extraSpecialArgs = { inherit inputs; };
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.liamdp = import ./theme/nord/desktops/hyprland/home/default.nix;
+	    };
           }
         ];
       };
