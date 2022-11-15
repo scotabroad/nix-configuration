35044c9f3380b0f42fbd54031df89bf67e2fe9e6
Liam Petrie <liamdpetrie@vivaldi.net>
2022-11-14 20:41:19 -0700

    Flake update

--- overlays/waybar.nix
+++ overlays/waybar.nix
@@ -2,19 +2,9 @@
 
 final: prev: {
   waybar = prev.waybar.overrideAttrs (oldAttrs: {
-    src = pkgs.fetchFromGitHub {
-      owner = "Alexays";
-      repo = "Waybar";
-      rev = "1374b0fce40f283b113e051f0558564fa1d9ad22";
-      sha256 = "UG233AXx7xU9O3/hs5mIXun7PCYibJvlDL1tQhB5gxE=";
-    };
     mesonFlags = oldAttrs.mesonFlags ++ [
       "-Dexperimental=true"
-      "-Djack=disabled"
-      "-Dlibinput=disabled"
     ];
-    
-    checkInputs = [ pkgs.catch2_3 ];
 
     #This patch is for hyprland only
     patches = (oldAttrs.patches or []) ++ [
