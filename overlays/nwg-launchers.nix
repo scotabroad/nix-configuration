fa37729f373b1957525e0587d0641a5c21474008
Liam Petrie <liamdpetrie@vivaldi.net>
2022-11-16 22:00:22 -0700

    Update nwg-launchers

--- overlays/nwg-launchers.nix
+++ overlays/nwg-launchers.nix
@@ -3,18 +3,14 @@
 final: prev: {
   nwg-launchers = prev.nwg-launchers.overrideAttrs (oldAttrs: {
     src = pkgs.fetchFromGitHub {
-      owner = "scotabroad";
+      owner = "nwg-piotr";
       repo = "nwg-launchers";
-      rev = "3387d9cd499c9319072ddc41ca32ac5a955a2dc1";
-      sha256 = "S6LEX74Zztvt7Di4VJq9gi91g6XjiReSfhCQQiJxe88=";
+      rev = "70f3390625c61ddf70ed688e36b64f434616a487";
+      sha256= "g453c9rrgibFcOIaTIGqKo1mVFypyfirFfdZpt4dKYg=";
     };
 
     buildInputs = oldAttrs.buildInputs ++ [
       pkgs.librsvg
     ];
-
-    #patches = (oldAttrs.patches or []) ++ [
-    #  ./nwg-launchers.patch
-    #];
   });
 }
