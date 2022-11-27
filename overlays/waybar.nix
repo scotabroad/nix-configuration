f0fa1bff84eea2702fd86e7e52b465d4f276294b
Liam Petrie <liamdpetrie@vivaldi.net>
2022-11-27 11:09:04 -0700

    Flake update

--- overlays/waybar.nix
+++ overlays/waybar.nix
@@ -2,6 +2,13 @@
 
 final: prev: {
   waybar = prev.waybar.overrideAttrs (oldAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "Alexays";
+      repo = "Waybar";
+      rev = "cfef78a5bc8df6a29e06ec128f7a6075425db402";
+      sha256 = "XMpl1soXKFe1NOux4HRBrmMrANgcnjGeGsUUKOzOFkE=";
+    };
+
     mesonFlags = oldAttrs.mesonFlags ++ [
       "-Dexperimental=true"
     ];
