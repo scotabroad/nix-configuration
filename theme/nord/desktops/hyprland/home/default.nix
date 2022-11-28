adbf57384079eb1f6ace0c6b9bd76c865fda0e09
Liam Petrie <liamdpetrie@vivaldi.net>
2022-11-27 19:37:06 -0700

    Added gradient to nord

--- theme/nord/desktops/hyprland/home/default.nix
+++ theme/nord/desktops/hyprland/home/default.nix
@@ -11,8 +11,10 @@
   wayland.windowManager.hyprland = {
     extraConfig = ''
       general {
-          col.active_border=0xff8fbcbb
-          col.inactive_border=0xff2e3440
+          #PARTY MODE!!!
+          #col.active_border= rgba(bf616aff) rgba(ebcb8bff) rgba(a3be8cff) rgba(88c0d0ff) rgba(81a1c1ff) rgba(b48eadff) 45deg
+          col.active_border=rgba(8fbcbbff) rgba(88c0d0ff) rgba(81a1c1ff) rgba(5e81acff) 45deg
+          col.inactive_border=rgba(2e3440ff) rgba(3b4252ff) rgba(434c5eff) rgba(4c566aff) rgba(434c5eff) rgba(3b4252ff) rgba(2e3440ff) 135deg
       }
     '';
   };
