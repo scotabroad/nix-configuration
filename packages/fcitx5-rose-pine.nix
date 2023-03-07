98089281a11c0559d7df996e78acda52339c11aa
Liam Petrie <liamdpetrie@vivaldi.net>
2023-03-07 14:16:31 -0700

    Moved Rosé-Pine theme to separate repo

--- packages/fcitx5-rose-pine.nix
+++ packages/fcitx5-rose-pine.nix
@@ -1,12 +1,18 @@
-{ stdenv }:
+{ fetchFromGitHub, stdenv }:
 
 stdenv.mkDerivation rec {
   pname = "fcitx5-rose-pine";
   version = "unstable";
 
-  src = ./fcitx5-rose-pine;
+  src = fetchFromGitHub {
+    owner = "rose-pine";
+    repo = "fcitx5";
+    rev = "148de09929c2e2f948376bb23bc25d72006403bc";
+    sha256 = "SpQ5ylHSDF5KCwKttAlXgrte3GA1cCCy/0OKNT1a3D8=";
+  };
 
   installPhase = ''
-    cp -r $src $out
+    mkdir $out
+    cp -r rose-pine* $out
   '';
 }
