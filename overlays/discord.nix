bf61dbce2c27a094275673f8bd0cb96f453459cd
Liam Petrie <liamdpetrie@vivaldi.net>
2023-11-01 14:43:50 -0700

    Update Discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -5,7 +5,7 @@ final: prev: {
     src = builtins.fetchTarball {
       url = "https://discord.com/api/download?platform=linux&format=tar.gz";
       #sha256 = "0000000000000000000000000000000000000000000000000000";
-      sha256 = "1pw9q4290yn62xisbkc7a7ckb1sa5acp91plp2mfpg7gp7v60zvz";
+      sha256 = "1mm470lahyk0cz9zd6pfv7zhlvaw5gvqip68867lj2lw63r4zivx";
     };
   });
 }
