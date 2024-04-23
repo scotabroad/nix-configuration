fcf5dd54ee8e0256561214e079aab7781cf093f7
Liam Petrie <liamdpetrie@vivaldi.net>
2024-04-23 16:13:59 -0700

    Refactor neovim configs

--- base/home/programs/neovim/default.nix
+++ base/home/programs/neovim/default.nix
@@ -10,12 +10,31 @@
       #    tree-sitter-nix
       #  ]
       #))
-      nvim-treesitter.withAllGrammars
+      { plugin = nvim-treesitter.withAllGrammars;
+        type = "lua";
+        config = ''
+          require('nvim-treesitter.configs').setup {
+            highlight = {
+              enable = true,
+            },
+          }
+        '';
+      }
       vim-lexurgy
       vim-numbertoggle
       vim-parinfer
     ];
+    viAlias = true;
+    vimAlias = true;
     vimdiffAlias = true;
+    extraConfig = ''
+      set number
+      autocmd FileType c setlocal et ts=4 sw=4
+      autocmd FileType cc setlocal et ts=4 sw=4
+      autocmd FileType cpp setlocal et ts=4 sw=4
+      autocmd FileType h setlocal et ts=4 sw=4
+      autocmd FileType hs setlocal et ts=4 sw=4
+      autocmd FileType nix setlocal et ts=2 sw=2
+    '';
   };
 }
-
