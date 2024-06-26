efc3d1fa1ad7a70b1c83d584ce49089255298364
Liam Petrie <liamdpetrie@vivaldi.net>
2024-06-26 10:57:27 -0600

    Add search engines to Firefox

--- base/home/programs/firefox/default.nix
+++ base/home/programs/firefox/default.nix
@@ -30,6 +30,7 @@
             urls = [{
               template = "https://search.nixos.org/options";
               params = [
+                { name = "channel"; value = "unstable"; }
                 { name = "type"; value = "options"; }
                 { name = "query"; value = "{searchTerms}"; }
               ];
@@ -42,6 +43,7 @@
             urls = [{
               template = "https://search.nixos.org/packages";
               params = [
+                { name = "channel"; value = "unstable"; }
                 { name = "type"; value = "packages"; }
                 { name = "query"; value = "{searchTerms}"; }
               ];
@@ -50,13 +52,37 @@
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@np" ];
           };
-          "NixOS Wiki" = {
+          "Unofficial NixOS Wiki" = {
             urls = [{
               template = "https://nixos.wiki/index.php?search={searchTerms}";
             }];
             iconUpdateURL = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@nu" ];
+          };
+          "Official NixOS Wiki" = {
+            urls = [{
+              template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+            }];
+            iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@nw" ];
+          };
+          "Wiktionary" = {
+            urls = [{
+              template = "https://en.wiktionary.org/wiki/{searchTerms}";
+            }];
+            iconUpdateURL = "https://en.wiktionary.org/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@wk" ];
+          };
+          "Wiktionary Latin" = {
+            urls = [{
+              template = "https://en.wiktionary.org/wiki/{searchTerms}#Latin";
+            }];
+            iconUpdateURL = "https://en.wiktionary.org/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@wl" ];
           };
         };
         force = true;
@@ -64,8 +90,11 @@
           "DuckDuckGo"
           "Nix Packages"
           "Nix Options"
-          "NixOS Wiki"
+          "Official NixOS Wiki"
+          "Unofficial NixOS Wiki"
           "Arch Wiki"
+          "Wiktionary"
+          "Wiktionary Latin"
         ];
       };
     };
